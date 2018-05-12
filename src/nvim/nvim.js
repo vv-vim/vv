1ecf3ca60c0381c3052eb7183841dc57a8e7ce23
Igor Gladkoborodov <igor.gladkoborodov@gmail.com>
2018-05-12 03:02:06 -0700

    Scroll, refactor

--- src/nvim/nvim.js
+++ src/nvim/nvim.js
@@ -1,53 +1,21 @@
-import {
-  initScreen,
-  cmdPut,
-  cmdCursorGoto,
-  cmdEolClear,
-  cmdHighlightSet,
-  cmdUpdateFg,
-  cmdUpdateBg,
-  cmdClear,
-} from './screen';
+import { initScreen, redrawCmd } from './screen';
 
-// const { ipcRenderer } = window.require('electron');
-//
-//
 const childProcess = global.require('child_process');
-// const remote = require('remote');
-// const Session = require('msgpack5rpc');
-
-
-const attach = global.require('neovim').attach;
+const { attach } = global.require('neovim');
 
 const handleNotification = (method, args) => {
-  // console.log('hey', method, args);
-  // console.log('nvim:notification', arg);
-  // const dateStart = Date.now();
   if (method === 'redraw') {
     for (let i = 0; i < args.length; i += 1) {
       const [cmd, ...props] = args[i];
-      // console.log('cmd', cmd, props);
-      if (cmd === 'put') {
-        cmdPut(props);
-      } else if (cmd === 'cursor_goto') {
-        cmdCursorGoto(props);
-      } else if (cmd === 'eol_clear') {
-        cmdEolClear();
-      } else if (cmd === 'highlight_set') {
-        cmdHighlightSet(props);
-      } else if (cmd === 'update_fg') {
-        cmdUpdateFg(props);
-      } else if (cmd === 'update_bg') {
-        cmdUpdateBg(props);
-      } else if (cmd === 'clear') {
-        cmdClear();
-      } else {
-        // console.log('Unknown =========', cmd);
+      try {
+        redrawCmd[cmd](props);
+      } catch (e) {
+        console.warn('Unknown redraw command', cmd, props); // eslint-disable-line no-console
       }
     }
+  } else {
+    console.warn('Unknown notification', method, args); // eslint-disable-line no-console
   }
-  // console.log(Date.now() - date, dateStart - date)
-  // console.log(`nvim:notification time=${Date.now() - d}`, arg);
 };
 
 const getKey = (event) => {
@@ -86,91 +54,29 @@ const getKey = (event) => {
   }
 };
 
-// let nvim
+let nvim;
 const handleKeydown = (event) => {
   const key = getKey(event);
   if (key) {
-    // ipcRenderer.send('nvim:input', key);
-    nvim.request('vim_input', [key], () => {});
+    nvim.request('vim_input', [key]);
   }
 };
 
-let nvim;
-
-async function initNvim() {
-  // const nvimArgs = ['--embed']; //.concat remote.process.argv[2..]
-
+async function initNvim(cols, rows) {
   const nvimProcess = childProcess.spawn('nvim', ['--embed', 'test/test.jsx'], {
     stdio: ['pipe', 'pipe', process.stderr],
   });
 
-  // Attach to neovim process
   nvim = await attach({ proc: nvimProcess });
 
-  nvim.request('ui_attach', [150, 50, true]);
+  nvim.request('ui_attach', [cols, rows, true]);
 
   nvim.on('notification', (method, args) => {
     handleNotification(method, args);
-  })
+  });
 
-  // nvim.command('vsp');
-  // nvim.command('vsp');
-  // nvim.command('vsp');
-  // const windows = await nvim.windows;
-  //
-  // // expect(windows.length).toEqual(4);
-  // // expect(windows[0] instanceof nvim.Window).toEqual(true);
-  // // expect(windows[1] instanceof nvim.Window).toEqual(true);
-  //
-  // nvim.window = windows[2];
-  // const win = await nvim.window;
-  //
-  // console.log('hey', win);
-  // // expect(win).not.toEqual(windows[0]);
-  // // expect(win).toEqual(windows[2]);
-  //
-  // const buf = await nvim.buffer;
-  // // expect(buf instanceof nvim.Buffer).toEqual(true);
-  //
-  // const lines = await buf.lines;
-  // // expect(lines).toEqual(['']);
-  //
-  // await buf.replace(['line1', 'line2'], 0);
-  // const newLines = await buf.lines;
-  // expect(newLines).toEqual(['line1', 'line2']);
-
-  // const readStream = fs.createReadStream(nvimProcess.stdout);
-  // const decodeStream = msgpack.createDecodeStream();
-  //
-  // // show multiple objects decoded from stream
-  // readStream.pipe(decodeStream).on('data', console.warn);
-
-  // console.log(nvimProcess.pid, nvimProcess.stdin, nvimProcess.stdout);
-
-  // const session = new Session();
-  // session.attach(nvimProcess.stdin, nvimProcess.stdout);
-  // session.on('notification', (method, args) => {
-  //   // console.log('notification', args);
-  //   handleNotification(args);
-  //   // mainWindow.webContents.send('nvim:notification', { method, args }, Date.now());
-  //   // @ui.handle_redraw args if method == 'redraw'
-  // });
-  // session.request('ui_attach', [150, 50, true], () => {
-  // console.log('ui attach');
-  //   ipcMain.on('nvim:input', (e, arg) => {
-  //     // console.log('vim_input');
-  //     session.request('vim_input', [arg], () => {});
-  //   });
-  //   // ui.on('input', (e) => session.request 'vim_input', [e], -> )
-  //   // ui.on('resize', (col, row) => session.request 'ui_try_resize', [col, row], () => {})
-  // });
-  //
-
-  // console.log('initvim');
-  initScreen(150, 50);
-  // ipcRenderer.on('nvim:notification', handleNotification);
-  // ipcRenderer.send('nvim:init');
+  initScreen(cols, rows);
   document.addEventListener('keydown', handleKeydown);
 }
 
-document.addEventListener('DOMContentLoaded', initNvim);
+document.addEventListener('DOMContentLoaded', () => initNvim(150, 50));
--- src/nvim/screen.js
+++ src/nvim/screen.js
@@ -1,116 +1,46 @@
-let container;
-let chars;
-let cursor;
+let cursor = [0, 0];
+const cursorElement = document.getElementById('cursor');
 let cols;
 let rows;
 let foregroundColor;
 let backgroundColor;
-let fgColor;
-let bgColor;
-let reverse;
+let defaultFgColor;
+let defaultBgColor;
 let context;
+let scrollRect;
 
-const chWidth = 7.2;
-const chHeight = 15;
-const fSize = 12;
+const colorsCache = {};
+const charsCache = {};
+
+const targetCharWidth = 7.2;
+const targetCharHeight = 15;
+const targetFontSize = 12;
+const fontFamily = 'SFMono-Light';
 const scale = 2;
-const charWidth = chWidth * scale;
-const charHeight = chHeight * scale;
-const fontSize = fSize * scale;
-const font = `${fontSize}px SFMono-Light`;
-
-let cursorElement;
-const charBitmaps = {};
-
-// let redrawCount = '1';
-// const redraw = () => {
-//   const d = Date.now();
-//   redrawCount = `${(parseInt(redrawCount, 10) + 1) % 10}`;
-//   for (let i = 0; i < rows; i += 1) {
-//     for (let j = 0; j < cols; j += 1) {
-//       printChar(i, j, redrawCount);
-//       // chars[i][j].nodeValue = redrawCount;
-//       // chars[i][j].innerHTML = redrawCount;
-//     }
-//   }
-//   // console.log('redraw=', Date.now() - d);
-// };
-// document.addEventListener('keydown', redraw);
-
-const createContainer = () => {
-  container = document.createElement('div');
-  document.getElementById('screen').appendChild(container);
-  return container;
-};
-
-export const cmdPut = (props) => {
-  for (let i = 0; i < props.length; i += 1) {
-    printChar(cursor[0], cursor[1], props[i][0]);
-    cursor[1] += 1;
-  }
-};
-
-export const cmdCursorGoto = (props) => {
-  cursor = props[0];
-  cursorElement.style.transform = `translate(${cursor[1] * 7.203 -
-    1}px, ${cursor[0] * 15 - 1}px)`;
-};
-
-export const cmdEolClear = () => {
-  for (let i = cursor[1]; i < cols; i += 1) {
-    printChar(cursor[0], i, ' ');
-  }
-};
-
-export const cmdClear = () => {
-  for (let i = 0; i < rows; i += 1) {
-    for (let j = 0; j < cols; j += 1) {
-      printChar(i, j, ' ');
-      // chars[i][j].nodeValue = redrawCount;
-      // chars[i][j].innerHTML = redrawCount;
-    }
-  }
-};
+const charWidth = targetCharWidth * scale;
+const charHeight = targetCharHeight * scale;
+const fontSize = targetFontSize * scale;
+const font = `${fontSize}px ${fontFamily}`;
 
 const getCharBitmap = (char) => {
-  const color = getForegroundColor();
-  const bgColor = getBackgroundColor();
-  const key = `${char}${color}${bgColor}`;
-  if (!charBitmaps[key]) {
+  const key = `${char}${foregroundColor}${backgroundColor}`;
+  if (!charsCache[key]) {
     const c = document.createElement('canvas');
     c.width = Math.ceil(charWidth);
     c.height = Math.ceil(charHeight);
-    const ctx = c.getContext('2d');
-    ctx.fillStyle = bgColor;
+    const ctx = c.getContext('2d', { alpha: false });
+    ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, Math.ceil(charWidth), Math.ceil(charHeight));
-    ctx.fillStyle = color;
+    ctx.fillStyle = foregroundColor;
     ctx.font = font;
     ctx.textBaseline = 'top';
     ctx.fillText(char, 0, 0);
-    charBitmaps[key] = c;
+    charsCache[key] = c;
   }
-  return charBitmaps[key];
+  return charsCache[key];
 };
 
 const printChar = (i, j, char) => {
-  // chars[i][j].firstChild.nodeValue = char;
-  // chars[i][j].style.color = getForegroundColor();
-  // chars[i][j].style.background = getBackgroundColor();
-  //
-  // context.fillStyle = getBackgroundColor();
-  // context.fillRect(
-  //   j * charWidth,
-  //   i * charHeight,
-  //   charWidth,
-  //   charHeight,
-  // );
-  // context.clearRect(
-  //   j * 7.201 * 2,
-  //   i * 15 * 2,
-  //   7.201 * 2,
-  //   15 * 2,
-  // );
-
   context.drawImage(
     getCharBitmap(char),
     Math.floor(j * charWidth),
@@ -118,64 +48,120 @@ const printChar = (i, j, char) => {
   );
 };
 
-const getBackgroundColor = () =>
-  (reverse ? foregroundColor || fgColor : backgroundColor || bgColor);
-
-const getForegroundColor = () =>
-  (reverse ? backgroundColor || bgColor : foregroundColor || fgColor);
-
 const getColorString = (rgb) => {
   if (!rgb) {
     return null;
   }
-  const bgr = [];
-  for (let i = 0; i < 3; i += 1) {
-    bgr.push(rgb & 0xff);
-    rgb >>= 8;
+  if (!colorsCache[rgb]) {
+    const bgr = [];
+    for (let i = 0; i < 3; i += 1) {
+      bgr.push(rgb & 0xff); // eslint-disable-line no-bitwise
+      rgb >>= 8; // eslint-disable-line no-param-reassign, no-bitwise
+    }
+    colorsCache[rgb] = `rgb(${bgr[2]},${bgr[1]},${bgr[0]})`;
   }
-  return `rgb(${bgr[2]},${bgr[1]},${bgr[0]})`;
+  return colorsCache[rgb];
 };
 
-export const cmdHighlightSet = (props) => {
-  for (let i = 0; i < props.length; i += 1) {
-    const { foreground, background, reverse: r } = props[i][0];
-    foregroundColor = getColorString(foreground);
-    backgroundColor = getColorString(background);
-    reverse = r;
-  }
-};
-
-export const cmdUpdateBg = (props) => {
-  bgColor = getColorString(props[0]);
-};
-
-export const cmdUpdateFg = (props) => {
-  fgColor = getColorString(props[0]);
-};
-
-const initCursorElement = () => {
-  cursorElement = document.createElement('span');
-  cursorElement.innerHTML = ' ';
-  cursorElement.setAttribute('id', 'cursor');
-  container.appendChild(cursorElement);
-};
-
-const initContext = () => {
+const initCanvas = () => {
   context = document
     .getElementById('canvas')
     .getContext('2d', { alpha: false });
-  context.font = '24px SFMono-Light';
-  context.textBaseline = 'top';
 };
 
 export const initScreen = (newCols, newRows) => {
   rows = newRows;
   cols = newCols;
-  cursor = [0, 0];
-  chars = [];
-  createContainer();
-  initCursorElement();
-  initContext();
+  initCanvas();
+};
+
+// https://github.com/neovim/neovim/blob/master/runtime/doc/ui.txt
+export const redrawCmd = {
+  put: (props) => {
+    for (let i = 0; i < props.length; i += 1) {
+      printChar(cursor[0], cursor[1], props[i][0]);
+      cursor[1] += 1;
+    }
+  },
+
+  cursor_goto: (props) => {
+    [cursor] = props;
+    cursorElement.style.transform = `translate(${(cursor[1] * targetCharWidth) -
+      1}px, ${(cursor[0] * 15) - 1}px)`;
+  },
+
+  eol_clear: () => {
+    for (let i = cursor[1]; i < cols; i += 1) {
+      printChar(cursor[0], i, ' ');
+    }
+  },
+
+  clear: () => {
+    for (let i = 0; i < rows; i += 1) {
+      for (let j = 0; j < cols; j += 1) {
+        printChar(i, j, ' ');
+      }
+    }
+  },
+
+  highlight_set: (props) => {
+    for (let i = 0; i < props.length; i += 1) {
+      const { foreground, background, reverse } = props[i][0];
+
+      const fg = getColorString(foreground);
+      const bg = getColorString(background);
+      foregroundColor = reverse ? bg || defaultBgColor : fg || defaultFgColor;
+      backgroundColor = reverse ? fg || defaultFgColor : bg || defaultBgColor;
+    }
+  },
+
+  update_bg: (props) => {
+    defaultBgColor = getColorString(props[0]);
+  },
+
+  update_fg: (props) => {
+    defaultFgColor = getColorString(props[0]);
+  },
+
+  set_scroll_region: (props) => {
+    [scrollRect] = props;
+    // top, bottom, left, right
+  },
+
+  scroll: (props) => {
+    const scrollCount = props[0];
+    const [top, bottom, left, right] = scrollRect;
+    // console.log('scroll count', scrollCount);
+    // console.log(top, bottom, left, right);
+    const rectCopy = context.getImageData(
+      left * charWidth,
+      (top + (scrollCount > 0 ? scrollCount : 0)) * charHeight,
+      (right - left + 1) * charWidth,
+      (bottom - top + 1 - (scrollCount > 0 ? scrollCount : -scrollCount)) * charHeight,
+    );
+    context.putImageData(
+      rectCopy,
+      left * charWidth,
+      (top + (scrollCount > 0 ? 0 : -scrollCount)) * charHeight,
+    );
+    if (scrollCount > 0) {
+      context.fillStyle = backgroundColor;
+      context.fillRect(
+        left,
+        (bottom + 1 - scrollCount) * charHeight,
+        (right - left + 1) * charWidth,
+        scrollCount * charHeight,
+      );
+    } else {
+      context.fillStyle = backgroundColor;
+      context.fillRect(
+        left,
+        top * charHeight,
+        (right - left + 1) * charWidth,
+        (top - scrollCount) * charHeight,
+      );
+    }
+  },
 };
 
 export default initScreen;
