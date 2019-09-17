b9fc5b4876ce743f6827d9d976045e0297fd0b78
Igor Gladkoborodov <igor.gladkoborodov@gmail.com>
2019-09-17 00:04:04 -0700

    Fix input composition symbols
    In insert mode when you type Alt+i, then u it should result with û. #19

--- src/renderer/input/insertSymbols.js
+++ src/renderer/input/insertSymbols.js
@@ -16,6 +16,9 @@ const initInsertSymbols = () => {
 
   (getCursorElement() || document.getElementsByTagName('body')[0]).appendChild(input);
 
+  let composition = false;
+  let compositionLength = 0;
+
   let insertedText = '';
   let ignoreNextInput = false;
   input.addEventListener('keydown', () => {
@@ -23,21 +26,42 @@ const initInsertSymbols = () => {
   });
 
   input.addEventListener('input', event => {
-    input.value = '';
-    if (ignoreNextInput) {
-      ignoreNextInput = false;
-      return;
+    if (!composition) {
+      input.value = '';
+      if (ignoreNextInput) {
+        ignoreNextInput = false;
+        return;
+      }
+      nvim.input(event.data.replace(insertedText, ''));
+      if (event.isComposing) {
+        insertedText = event.data;
+      }
     }
-    nvim.input(event.data.replace(insertedText, ''));
-    if (event.isComposing) {
-      insertedText = event.data;
+  });
+
+  // Composition symbols, for example Alt+i, then u will make û
+  input.addEventListener('compositionstart', () => {
+    composition = true;
+    compositionLength = 0;
+  });
+
+  input.addEventListener('compositionupdate', event => {
+    if (compositionLength > 0) {
+      nvim.input('<BS>'.repeat(compositionLength));
     }
+    compositionLength = event.data.length;
+    nvim.input(event.data);
+  });
+
+  input.addEventListener('compositionend', () => {
+    composition = false;
   });
 
   nvim.on('redraw', args => {
     for (let i = 0; i < args.length; i += 1) {
       const [cmd, ...props] = args[i];
       if (cmd === 'mode_change') {
+        // TODO: check other modes https://github.com/neovim/neovim/blob/master/src/nvim/cursor_shape.c#L18
         if (['insert', 'replace', 'cmdline_normal'].includes(props[0][0])) {
           input.value = '';
           input.focus();
